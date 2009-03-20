52ccf2d571244225754363316e9cbd989718800b
lp <lp@spiralix.org>
2009-03-20 08:53:10 -0400

    bit more shoes native widgets

--- lib/cordovan_native.rb
+++ lib/cordovan_native.rb
@@ -4,6 +4,18 @@
 # :title:Cordovan
 class Cordovan
 	
+	def arc(style={})
+		in_grid(style) do |props|
+			@shoes.arc(props)
+		end
+	end
+	
+	def arrow(style={})
+		in_grid(style) do |props|
+			@shoes.arrow(props)
+		end
+	end
+	
 	def background(color,style={})
 		@shoes.background(color,style={})
 	end
@@ -16,6 +28,10 @@ class Cordovan
 		in_grid(style) do |props|
 			@shoes.button(text,props) { yield if block_given? }
 		end
+	end
+	
+	def cap(type)
+		@shoes.cap(type)
 	end
 	
 	def check(style={})
@@ -36,6 +52,16 @@ class Cordovan
 		end
 	end
 	
+	def fill(color)
+		@shoes.fill(color)
+	end
+	
+	def flow(style={})
+		in_grid(style) do |props|
+			@shoes.flow(props)
+		end
+	end
+	
 	def image(url,style={})
 		in_grid(style) do |props|
 			@shoes.image(url,props)
@@ -45,6 +71,18 @@ class Cordovan
 	def list_box(style={})
 		in_grid(style) do |props|
 			@shoes.list_box(props) { yield if block_given? }
+		end
+	end
+	
+	def oval(style={})
+		in_grid(style) do |props|
+			@shoes.oval(props)
+		end
+	end
+	
+	def para(text,style={})
+		in_grid(style) do |props|
+			@shoes.para(text,props)
 		end
 	end
 	
@@ -66,22 +104,14 @@ class Cordovan
 		end
 	end
 	
-	def para(text,style={})
-		in_grid(style) do |props|
-			@shoes.para(text,props)
-		end
-	end
-	
-	def flow(style={})
-		in_grid(style) do |props|
-			@shoes.flow(props)
-		end
-	end
-	
 	def stack(style={})
 		in_grid(style) do |props|
 			@shoes.stack(props) { yield if block_given? }
 		end
+	end
+	
+	def stroke(color)
+		@shoes.stroke(color)
 	end
 	
 end
