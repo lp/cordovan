2141b3028c080f8a709d040fdeb5d67c2659b3b4
lp <lp@spiralix.org>
2009-03-21 11:14:50 -0400

    shoes color as cordovan methods

---
+++ lib/cordovan_color.rb
@@ -0,0 +1,16 @@
+# Author:: lp (mailto:lp@spiralix.org)
+# Copyright:: 2009 Louis-Philippe Perron - Released under the terms of the MIT license
+# 
+# :title:Cordovan
+class Cordovan
+	
+	def red
+		@shoes.red
+	end
+	
+	def blue
+		@shoes.blue
+	end
+	
+	
+end
