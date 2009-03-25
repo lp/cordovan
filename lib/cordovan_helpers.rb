23416fd24ec4f6433f9442ce7880161bacdf60f2
lp <lp@spiralix.org>
2009-03-25 09:23:45 -0400

    also supports ratio for positionning and size

--- lib/cordovan_helpers.rb
+++ lib/cordovan_helpers.rb
@@ -8,8 +8,8 @@ class Cordovan
 	def fit_in_grid(style)
 		style[:v_span] = @v_grid if style[:v_span].nil?
 		style[:h_span] = @h_grid if style[:h_span].nil?
-		style[:v_pos] = 1 if style[:v_pos].nil? || style[:v_pos] < 1
-		style[:h_pos] = 1 if style[:h_pos].nil? || style[:h_pos] < 1
+		style[:v_pos] = 1 if style[:v_pos].nil?
+		style[:h_pos] = 1 if style[:h_pos].nil?
 		style[:height] = height_from(style[:v_span])
 		style[:width] = width_from(style[:h_span])
 		style[:left] = x_from(style[:h_pos])
@@ -18,18 +18,22 @@ class Cordovan
 	end 
 
 	def height_from(v_span)
+		return (@height * v_span).to_i if v_span.is_a?(Float) and v_span <= 1.0
 		(@height.to_f / @v_grid * v_span).to_i
 	end
 
 	def width_from(h_span)
+		return (@width * h_span).to_i if h_span.is_a?(Float) and h_span <= 1.0
 		(@width.to_f / @h_grid * h_span).to_i
 	end
 
 	def x_from(h_pos)
+		return (@width * h_pos).to_i if h_pos.is_a?(Float) and h_pos <= 1.0
 		(@width.to_f / @h_grid * (h_pos-1)).to_i
 	end
 	
 	def y_from(v_pos)
+		return (@height * v_pos).to_i if v_pos.is_a?(Float) and v_pos <= 1.0
 		(@height.to_f / @v_grid * (v_pos-1)).to_i
 	end
 	
